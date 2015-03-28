6b952e38c1eb933f18d8da86b8e56192ae1b9ce2
Roberto Amorim <rjamorim@yahoo.com>
2015-03-28 15:34:34 -0400

    Cursor detection ready. Start readme file

--- __init__.py
+++ __init__.py
@@ -1,14 +1,28 @@
 import numpy as np
 import cv2
+import cv
 
+# Get the videoCapture object and set it a bit ahead, so we don't have to wait a lot to start debugging
 cap = cv2.VideoCapture("../resources/test.mp4")
+cap.set(cv.CV_CAP_PROP_POS_MSEC, 27500)
 
-while cap.isOpened():
+# Read the first frame to obtain the video dimension
+ret, frame = cap.read()
+
+# Buffers we will use throughout our application
+frameThresh = np.zeros(frame.shape, np.uint8)
+
+# Main loop
+while ( cap.isOpened() and cv2.waitKey(50) != 27 ):
     ret, frame = cap.read()
 
+    # Identifies positions within the frame with color values between 230 and 255 to locate the cursor
+    frameThresh = cv2.inRange(frame, (230, 230, 230), (255, 255, 255))
+
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv2.imshow('frameThresh', frameThresh)
+
 
 cap.release()
 cv2.destroyAllWindows()
+
