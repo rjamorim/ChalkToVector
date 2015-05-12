fc0de36ca51ae33f442ecde8568d4ffd91b80109
Roberto Amorim <rjamorim@yahoo.com>
2015-05-12 02:32:57 -0400

    Cursor parsing functions

---
+++ cursorParser.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2
+import cv
+
+
+# Method to store in a file a list of all cursor positions in the video file
+def updateCursorPosList(videofile):
+
+    cap = cv2.VideoCapture(videofile)
+
+    contoursFile = open("./resources/contours.txt", 'w')
+
+    while cap.isOpened():
+
+        ret, frame = cap.read()
+
+        if frame == None: break
+
+        # Identifies positions within the frame with color values between 230 and 255 to locate the cursor
+        frameThresh = cv2.inRange(frame, (230, 230, 230), (255, 255, 255))
+        contours = cv2.findContours(frameThresh, cv.CV_RETR_EXTERNAL, cv.CV_CHAIN_APPROX_NONE)[0]
+
+        maxArea = 0
+        maxCnt = None
+
+        # Getting cursor position from its contour
+        if contours:
+
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if area > maxArea:
+                    maxArea = area
+                    maxCnt = cnt
+
+            if maxArea:
+                cursorCoord = (100000, 100000)  # Just a number much larger than any video dimension
+                for vert in maxCnt:
+                    if vert[0][0] <= cursorCoord[0] and vert[0][1] <= cursorCoord[1]:
+                        cursorCoord = (vert[0][0]-1, vert[0][1]-3)
+
+                contoursFile.write(str(cursorCoord[0]) + " " + str(cursorCoord[1]) + "\n")
+
+        if not maxArea or not contours:
+            contoursFile.write("-1 -1\n")
+
+    contoursFile.close()
+
+
+# Returns an array of tuples with the list of all cursor positions in the video
+def getCursorPosList():
+
+    cursorPosList = []
+
+    with open("./resources/contours.txt", "r") as f:
+        line = f.readline()
+        while line:
+            tmp = line.split(' ')
+            cursorPosList.append( (tmp[0], tmp[1]) )
+            line = f.readline()
+
+    return cursorPosList
